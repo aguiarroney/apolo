bc7ca3f8892bd96464b89fe0008ec9d65054dd21
Roney Aguiar <aguiarroney@gmail.com>
2021-02-01 08:22:52 -0300

    melhoria no navController para evitar erro de troca de configuração

--- app/src/main/java/com/example/apolo/MainActivity.kt
+++ app/src/main/java/com/example/apolo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.apolo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.apolo.databinding.ActivityMainBinding
 
@@ -12,13 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bnvNavigationView.setupWithNavController(
-            Navigation.findNavController(
-                this,
-                R.id.nav_host_fragment
-            )
-        )
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bnvNavigationView.setupWithNavController(
+            navController
+        )
     }
 
 }
